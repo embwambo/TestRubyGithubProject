3f0f69fe127359d56f823b3159dbcb4c4f5fce62
Emmanuel Mbwambo <emmanuelmbwambo@gmail.com>
2015-03-24 18:04:15 +0300

    Ternary Operator

---
+++ ternaryConditionOperator.rb
@@ -0,0 +1,12 @@
+if 40 > 6
+  puts "This is right"
+  
+else
+  puts "This is wrong "
+end
+
+puts 40 > 6 ? "This is right" : "This is wrong"
+
+puts "This is right" if 40 > 6
+
+puts "This is right" unless 40 < 6
